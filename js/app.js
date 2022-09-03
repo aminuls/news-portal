e2670959f06aaba7650a1880722098e95db9052f
aminul <accounreate@gmail.com>
2022-09-03 11:46:48 +0600

    category added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 const navUl = () => {
    const ul = document.getElementsByClassName("nav-ul");
    const navItems = ["News", "Blog", "About", "Terms"];
-   console.log(ul);
-   for(let i=0; i<ul.length; i++){
+   for (let i = 0; i < ul.length; i++) {
       navItems.forEach((navItem) => {
          const li = document.createElement("li");
          li.innerHTML = `
@@ -13,3 +12,20 @@ const navUl = () => {
    }
 };
 navUl();
+
+const categoryFetch = async () => {
+   const url = `https://openapi.programming-hero.com/api/news/categories`;
+   const res = await fetch(url);
+   const data = await res.json();
+   categoryShow(data.data.news_category);
+};
+const categoryShow = (data) => {
+   const aCategory = [{ category_id: "00", category_name: "Home" }, ...data];
+   const categoryUl = document.getElementById("category-ul");
+   aCategory.forEach((one) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<a>${one.category_name}</a>`;
+      categoryUl.appendChild(li);
+   });
+};
+categoryFetch();
